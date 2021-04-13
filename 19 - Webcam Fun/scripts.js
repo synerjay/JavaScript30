b618329406f10c479443d6e326b713c27db7ac60
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-13 10:22:28 +0900

    Added red and rgbsplit filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,6 +26,16 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    //take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //mess with them
+
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.8;
+
+    //put them back into the canvas
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -41,6 +51,33 @@ function takePhoto() {
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
 }
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+// function rgbSplit(pixels) {
+//   for (let i = 0; i < pixels.data.length; i += 4) {
+//     pixels.data[i - 150] = pixels.data[i + 0]; // RED
+//     pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+//     pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+//   }
+//   return pixels;
+// }
 
 getVideo();
 
