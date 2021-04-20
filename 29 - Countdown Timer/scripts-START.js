c125fbeb73ea48030c551382fd070f1bc50327e1
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-20 21:05:23 +0900

    yay

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,11 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
-
+const endTime = document.querySelector('.display__end-time');
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -27,4 +28,13 @@ function displayTimeLeft(seconds) {
   timerDisplay.textContent = display;
   document.title = display;
   console.log({ minutes, remainderSeconds });
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`;
 }
